a26839f75df01db23dbcddf931d531313a6e2ef5
Vishal S <65108964+blog-a1@users.noreply.github.com>
2021-10-03 17:05:42 +0530

    contributed as per #2

---
+++ tower_of_hanoi.c
@@ -0,0 +1,43 @@
+//Using Recursion
+
+#include <stdio.h>
+void towers(int num, char frompeg, char topeg, char auxpeg)
+{
+    if (num == 1)
+    {
+        printf("\n Move disk 1 from peg %c to peg %c", frompeg, topeg);
+        return;
+    }
+    towers(num - 1, frompeg, auxpeg, topeg);
+    printf("\n Move disk %d from peg %c to peg %c", num, frompeg, topeg);
+    towers(num - 1, auxpeg, topeg, frompeg);
+}
+
+
+int main()
+{
+    int num;
+ 
+    printf("Enter the number of disks : ");
+    scanf("%d", &num);
+    printf("The sequence of moves involved in the Tower of Hanoi are :\n");
+    towers(num, 'A', 'C', 'B');
+    return 0;
+}
+
+/*
+INPUT 
+Enter the number of disks : 3
+
+OUTPUT
+The sequence of moves involved in the Tower of Hanoi are :
+ 
+Move disk 1 from peg A to peg C
+Move disk 2 from peg A to peg B
+Move disk 1 from peg C to peg B
+Move disk 3 from peg A to peg C
+Move disk 1 from peg B to peg A
+Move disk 2 from peg B to peg C
+Move disk 1 from peg A to peg C
+
+*/
