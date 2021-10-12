f178e9f76b85e86d404b6753425d2bb14686f4f3
vikramoff24 <vikram.senthil@outlook.com>
2021-10-12 13:49:33 +0530

    added programs

---
+++ Palindromic String.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+bool isPalin(char a[])
+{
+	int i=0; 
+	int j=strlen(a)-1;
+	
+	while(i<j)
+	{
+		
+		if(a[i]==a[j])
+		{
+			i++;
+			j--;
+		}
+		else
+		{
+			return false;
+		}
+	}
+	return true;
+}
+int main()
+{
+	char a[100];
+cin.getline(a,100);
+
+if(isPalin(a)) cout<<"String is a palindrome";
+else cout<<"String is not palindrome";
+
+	return 0;
+}
